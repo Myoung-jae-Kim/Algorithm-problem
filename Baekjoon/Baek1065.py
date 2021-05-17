0d968191af074afa5008020414de39708408be1f
Kim-Myoung-jae <rlaaud30@naver.com>
2021-05-17 23:05:07 +0900

    done

---
+++ Baekjoon/Baek1065.py
@@ -0,0 +1,16 @@
+# 1065 한 수
+
+num = int(input())
+result = 0
+
+for n in range(1, num+1) :
+    if n <= 99 : # 1부터 99까지는 모두 한수
+        result += 1 
+    
+    else :     
+        nums = list(map(int, str(n))) # 숫자를 자릿수대로 분리 
+        if nums[0] - nums[1] == nums[1] - nums[2] : #등차수열 확인
+            result+=1
+
+
+print(result)
