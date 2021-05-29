7c8c3f389f8a54554dc7e7b4fe581766d663a0bf
Kim-Myoung-jae <rlaaud30@naver.com>
2021-05-29 16:48:07 +0900

    done :-)

---
+++ Baekjoon/Baek10815.py
@@ -0,0 +1,31 @@
+#숫자 카드
+from sys import stdin
+
+def binary_search(Num_list,Num):
+    low = 0
+    high = len(Num_list) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+
+        if (Num_list[mid] == Num):
+            return 1 
+        elif (Num_list[mid] < Num):
+            low = mid + 1
+        else:
+            high = mid - 1
+    return 0
+
+N = int(stdin.readline())
+N_list = list(map(int, input().split()))
+
+M = int(stdin.readline())
+M_list = list(map(int, input().split()))
+
+#이진탐색을 위한 리스트 정렬
+N_list.sort()
+
+for i in range(M):
+    print(binary_search(N_list,M_list[i]), end = ' ')
+        
+    
