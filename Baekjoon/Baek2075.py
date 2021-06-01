1ddf8c1dbaf4d392ba86394a644e9f0ae62a7571
Kim-Myoung-jae <rlaaud30@naver.com>
2021-06-01 16:07:50 +0900

    done

---
+++ Baekjoon/Baek2075.py
@@ -0,0 +1,33 @@
+#N번째 큰 수
+import sys
+import heapq
+
+N = int(input())
+N2list = []
+result = []
+
+for _ in range(N):
+    N2list = list(map(int, sys.stdin.readline().split()))
+
+    if result:
+        for i in N2list:
+            if result[0] < i:
+                heapq.heappush(result, i)
+                heapq.heappop(result)
+    
+    if not result:
+        for i in N2list:
+            heapq.heappush(result, i)
+
+
+
+#print(N2list)
+
+# for i in range(N):
+#     for j in range(N):
+#         result.append(N2list[i][j])
+
+#result.sort()
+
+#print(result)
+print(result[0])
