95e2f0cd8723a200a82c7d69b59245da2e5deed9
Kim-Myoung-jae <rlaaud30@naver.com>
2021-07-18 14:56:49 +0900

    done

--- Baekjoon/Baek17299.py
+++ Baekjoon/Baek17299.py
@@ -1,25 +1,23 @@
-from sys import stdin
-from collections import Counter
+#Baekjoon 17299번 오등큰수
 
-if __name__ == "__main__":
-    stack = []
+import sys
 
-    n = int(stdin.readline())
-    s = list(stdin.readline().split())
-    cnt = Counter(s)
-    s = [[cnt[num], int(num)] for num in s]
-    answer = [-1 for _ in range(n)]
-    stack.append(0)
+num = int(sys.stdin.readline())
+a = list(map(int, sys.stdin.readline().split()))
+result = ["-1" for _ in range(num)]
+stack = [0]
+count = dict()
+for i in a:
+    try:
+        count[i] += 1
+    except:
+        count[i] = 1
 
-    i = 1
+for i in range(num):
+    while stack and count[a[stack[-1]]] < count[a[i]]:
+        result[stack[-1]] = str(a[i])
+        stack.pop()
+    stack.append(i)
+    i+=1
 
-    while stack and i < n:
-        while stack and s[stack[-1]][0] < s[i][0]:
-            answer[stack[-1]] = s[i][1]
-            stack.pop()
-
-        stack.append(i)
-        i += 1
-
-    for num in answer:
-        print(num, end=' ')
+print(" ".join(result))
